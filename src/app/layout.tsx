1f889bf80ac1c63ec4c9093f3ef64af80fdd0447
Abhishek Singh <abhsin96@gmail.com>
2026-06-06 22:42:21 +0530

    feat: add preconnect links to Google Fonts for improved loading performance

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,6 +29,14 @@ export default function RootLayout({
   return (
     <html lang="en" className={cn(geistSans.variable, geistMono.variable)}>
       <head>
+        {/* Preconnect to Google Fonts to reduce discovery time */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+
         <link
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap"
           rel="stylesheet"
